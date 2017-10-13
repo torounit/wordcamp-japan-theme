fe582bdce76561d85994db684c41bb736275d710
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2017-10-14 03:35:04 +0900

    fix format

--- functions.php
+++ functions.php
@@ -1,6 +1,16 @@
 <?php
-function wordcamp_japan_theme_css() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/wordcamp-japan-theme.css', array('parent-style'));
+/**
+ * Components functions and definitions.
+ *
+ * @package wordcamp_japan_theme
+ */
+
+/**
+ * Enqueue stylesheets.
+ */
+function wordcamp_japan_theme_style() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'wordcamp-japan-theme-style', get_stylesheet_directory_uri() . '/css/wordcamp-japan-theme.css', array( 'parent-style' ) );
 }
-add_action( 'wp_enqueue_scripts', 'wordcamp_japan_theme_css' );
+
+add_action( 'wp_enqueue_scripts', 'wordcamp_japan_theme_style' );
